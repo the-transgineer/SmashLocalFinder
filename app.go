5a7f54eb06fea99ec6962a7733befbeed2be4111
McKinley Forbes <mckinley.forbes95@gmail.com>
2019-06-13 07:45:07 -0500

    added config to app.go

--- app.go
+++ app.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	. "SmashLocalFinder/config"
+	. "SmashLocalFinder/dao"
+	. "SmashLocalFinder/models"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
 )
 
+var dao = LocalsDAO{}
+var config = Config{}
+
 func AllLocals(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	locals, err := dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, locals)
 }
 
 func FindLocal(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +30,18 @@ func FindLocal(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateLocal(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var local Local
+	if err := json.NewDecoder(r.Body).Decode(&local); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	local.ID = bson.NewObjectId()
+	if err := dao.Insert(local); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, local)
 }
 
 func UpdateLocal(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +50,24 @@ func UpdateLocal(w http.ResponseWriter, r *http.Request) {
 
 func DeleteLocal(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func init() {
+	config.Read()
+	dao.Server = config.Server
+	dao.Database = config.Databse
+	dao.Connect()
 }
 
 func main() {
