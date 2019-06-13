3a6129b983c016ce0634e02f17c5ed06f8d6ca65
McKinley Forbes <mckinley.forbes95@gmail.com>
2019-06-12 23:34:58 -0500

    initial commit

---
+++ app.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Local struct {
+	ID        bson.ObjectId `bson:"_id" json:"id"`
+	Name      string        `bson:"name" json:name`
+	Day       string        `bson:"day" json:"day"`
+	Time      time.Time     `bson:"time" json:"time"`
+	Location  string        `bson:"location" json:"location"`
+	Organizer string        `bson:"orgranizer" json:"organizer"`
+}
+
+func AllLocals(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func FindLocal(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func CreateLocal(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func UpdateLocal(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func DeleteLocal(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/locals", AllLocals).Methods("GET")
+	r.HandleFunc("/locals", CreateLocal).Methods("POST")
+	r.HandleFunc("/locals", UpdateLocal).Methods("PUT")
+	r.HandleFunc("/locals", DeleteLocal).Methods("DELETE")
+	r.HandleFunc("/locals/{id}", FindLocal).Methods("GET")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
