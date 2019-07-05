92f4c6f35f31349f22cd2aee4263c3dad5e7f6b0
McKinley Forbes <mckinley.forbes95@gmail.com>
2019-07-05 03:32:53 -0500

    fixed router to use new routes

--- frontend/src/Components/App/router.jsx
+++ frontend/src/Components/App/router.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
 import Regions from "../../Routes/Regions";
 import Region from "../../Routes/Region";
+import Admin from "../../Routes/Admin";
+import Create from "../../Routes/Create";
 
 const Router = () => (
     <BrowserRouter>
       <Route component={Regions} exact path="/regions"/>
       <Route component={Region} path="/regions/:region" />
+      <Route component={Admin} path="/admin/"/>
+      <Route component={Create} path="/admin/create"/>
     </BrowserRouter>
 );
 
