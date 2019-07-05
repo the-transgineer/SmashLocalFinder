925b44a1b970820d9c6619b63a661d459ffb404a
McKinley Forbes <mckinley.forbes95@gmail.com>
2019-07-05 03:32:13 -0500

    added admin toolbar

---
+++ frontend/src/Routes/Admin/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+        <div className="Admin">
+            <Link to="/admin/create">
+                Create
+            </Link>
+        </div>
+)
